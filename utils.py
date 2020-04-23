a578f15d84a6a844858a79d236c6d5cc3dc0d72d
Naoto Nishizaka <nszk.nao@gmail.com>
2020-04-23 16:19:02 +0900

    add

--- utils.py
+++ utils.py
@@ -9,13 +9,34 @@ n = int(input())
 a, b = map(int, input().split(' '))
 
 ##########
+### データ型
+##########
+s = set()
+s.add()
+
+##########
 ### よく使う関数
 ##########
 # 商と余り
 q, mod = divmod(10, 3)
+# nCr
+from operator import mul
+from functools import reduce
+def cmb(n,r):
+    r = min(n-r,r)
+    if r == 0: return 1
+    over = reduce(mul, range(n, n - r, -1))
+    under = reduce(mul, range(1,r + 1))
+    return over // under
 # 組み合わせを列挙
 import itertools
 for v in itertools.combinations([1,2,3], 2):
+    # vはタプル
+    # もとのlistの順番は崩れない
+    print(v)
+# デカルト積
+import itertools
+for v in itertools.product([1,2,3], [1,2,3]):
     # vはタプル
     print(v)
 # 約数を列挙
