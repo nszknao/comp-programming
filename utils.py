3701d7b19c50389d3f4f59df9e44a434cc7282a7
Naoto Nishizaka <nszk.nao@gmail.com>
2020-04-27 19:32:07 +0900

    update

--- utils.py
+++ utils.py
@@ -9,6 +9,12 @@ n = int(input())
 a, b = map(int, input().split(' '))
 
 ##########
+### 基本
+##########
+keys = [1,2,3]
+dicts = {key:'1' for key in keys}
+
+##########
 ### データ型
 ##########
 s = set()
@@ -48,3 +54,19 @@ def make_divisors(n):
             if i != n // i:
                 divisors.append(n//i)
     return divisors
+# ビットカウント
+def popcount(x):
+    '''xの立っているビット数をカウントする関数
+    (xは64bit整数)'''
+
+    # 2bitごとの組に分け、立っているビット数を2bitで表現する
+    x = x - ((x >> 1) & 0x5555555555555555)
+
+    # 4bit整数に 上位2bit + 下位2bit を計算した値を入れる
+    x = (x & 0x3333333333333333) + ((x >> 2) & 0x3333333333333333)
+
+    x = (x + (x >> 4)) & 0x0f0f0f0f0f0f0f0f # 8bitごと
+    x = x + (x >> 8) # 16bitごと
+    x = x + (x >> 16) # 32bitごと
+    x = x + (x >> 32) # 64bitごと = 全部の合計
+    return x & 0x0000007f
